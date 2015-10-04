77b96843f8a91eb932f9611618dedba969d5db5d
jmantas <jmantas@users.noreply.github.com>
2015-10-04 17:21:26 +0300

    Comments

--- eth-node-info-cli.py
+++ eth-node-info-cli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- mode: python; coding: utf-8 -*-
 
-
+''' CLI tool to get basic info from ethereum via JSON-RPC '''
 
 import json
 import requests
@@ -9,10 +9,10 @@ import argparse
 
 
 def main():
-
+    ''' Main function '''
     parser = argparse.ArgumentParser(add_help=True)
-    parser.add_argument("--host", dest="jrpc_host", default="127.0.0.1")
-    parser.add_argument("--port", dest="jrpc_port", default="18801")    
+    parser.add_argument("--host", dest="jrpc_host")
+    parser.add_argument("--port", dest="jrpc_port")
     parser.add_argument("--version", action="store_true")
     parser.add_argument("--protocol", action="store_true")
     parser.add_argument("--networkid", action="store_true")
@@ -27,12 +27,12 @@ def main():
     parser.add_argument("--balance", dest="account_balance", type=str)
     parser.add_argument("--txcount", dest="account_txcount", type=str)
     args = parser.parse_args()
-    
+
     eth_instance = EthClient(args.jrpc_host, args.jrpc_port)
-   
+
     if args.version:
         print "Node version:{}".format(eth_instance.node_version())
-    
+
     if args.protocol:
         print "Protocol version:{}".format(eth_instance.protocol_version())
 
@@ -41,19 +41,19 @@ def main():
 
     if args.listening:
         print "Nerwork listening:{}".format(eth_instance.network_is_listening())
-    
+
     if args.peercount:
         print "Peer count:{}".format(eth_instance.network_peer_count())
 
     if args.mining:
         print "Node mining:{}".format(eth_instance.node_is_mining())
-    
+
     if args.coinbase:
         print "Coinbase:{}".format(eth_instance.coinbase())
 
     if args.hashrate:
         print "Hashrate:{}".format(eth_instance.hashrate())
-    
+
     if args.gasprice:
         print "Gasprice:{}".format(eth_instance.gas_price())
 
@@ -61,7 +61,7 @@ def main():
         print "Accounts:{}".format(eth_instance.accounts())
 
     if args.blocknumber:
-        print "Current block number:{}".format(eth_instance.current_block_number())
+        print "Last block number:{}".format(eth_instance.current_block_number())
 
     if args.account_balance:
         acc = args.account_balance
@@ -74,7 +74,7 @@ def main():
         print "Transaction count for {} is:{}".format(acc, txcount)
 
 class EthClient(object):
-
+    ''' eth connection initialization '''
     json_id = 0
 
     def __init__(self, host, port):
@@ -83,10 +83,12 @@ class EthClient(object):
         self.session = requests.session()
 
     def json_id_index(self):
+        ''' Iterator '''
         self.json_id += 1
         return self.json_id
 
     def init_rpc_request(self, rpc_method, json_parameters):
+        ''' JSON-RPC connector '''
         json_data = self.session.post('http://{host}:{port}/'.format
         (host=self.json_rpc_host, port=self.json_rpc_port),
         data=json.dumps({
@@ -103,26 +105,31 @@ class EthClient(object):
 
 
     def node_version(self):
+        ''' get node version '''
         rpc_request_output = self.init_rpc_request(
             "web3_clientVersion", [])
         return rpc_request_output['result']
 
     def protocol_version(self):
+        ''' get protocol version '''
         rpc_request_output = self.init_rpc_request(
             "eth_protocolVersion", [])
         return rpc_request_output['result']
 
     def network_id(self):
+        ''' get network ID '''
         rpc_request_output = self.init_rpc_request(
             "net_version", [])
         return rpc_request_output['result']
 
     def network_is_listening(self):
+        ''' listen status '''
         rpc_request_output = self.init_rpc_request(
             "net_listening", [])
         return rpc_request_output['result']
 
     def network_peer_count(self):
+        ''' get peer count '''
         rpc_request_output = self.init_rpc_request(
             "net_peerCount", [])
         return int(rpc_request_output['result'], 16)
@@ -134,42 +141,49 @@ class EthClient(object):
 
 
     def node_is_mining(self):
+        ''' mining status '''
         rpc_request_output = self.init_rpc_request(
             "eth_mining", [])
         return rpc_request_output['result']
 
     def coinbase(self):
+        ''' get coinbase '''
         rpc_request_output = self.init_rpc_request(
             "eth_coinbase", [])
         return rpc_request_output['result']
 
-
     def hashrate(self):
+        ''' get hasrate '''
         rpc_request_output = self.init_rpc_request(
             "eth_hashrate", [])
         return int(rpc_request_output['result'], 16)
 
     def gas_price(self):
+        ''' get gas price '''
         rpc_request_output = self.init_rpc_request(
             "eth_gasPrice", [])
         return int(rpc_request_output['result'], 16)
 
     def accounts(self):
+        ''' get list of accounts '''
         rpc_request_output = self.init_rpc_request(
             "eth_accounts", [])
         return rpc_request_output['result']
-    
+
     def current_block_number(self):
+        ''' get latest block number '''
         rpc_request_output = self.init_rpc_request(
             "eth_blockNumber", [])
         return int(rpc_request_output['result'], 16)
 
     def balance(self, account, block_number="latest"):
+        ''' get balance for account hash, by default at latest synced block number '''
         rpc_request_output = self.init_rpc_request(
             "eth_getBalance", [account, block_number])
         return int(rpc_request_output['result'], 16)
 
     def transaction_count(self, account, block_number="latest"):
+        ''' get transaction count for account hash '''
         rpc_request_output = self.init_rpc_request(
             "eth_getTransactionCount", [account, block_number])
         return int(rpc_request_output['result'], 16)
@@ -192,13 +206,13 @@ class EthClient(object):
         rpc_request_output = self.init_rpc_request(
             "eth_getUncleCountByBlockHash", [block_hash])
         return int(rpc_request_output['result'], 16)
-    
+
     def uncle_count_from_block_number(self, block_number):
         rpc_request_output = self.init_rpc_request(
             "eth_getUncleCountByBlockNumber", [block_number])
 
         return int(rpc_request_output['result'], 16)
-    
+
     def code_from_address_hash(self, address, block_number="latest"):
         rpc_request_output = self.init_rpc_request(
             "eth_getCode", [address, block_number])
@@ -207,7 +221,7 @@ class EthClient(object):
 
 #    how to unlock with RPC?? not working when unlocked in geth console
 #    def sign_data(self, address, data):
-#        rpc_request_output = self.init_rpc_request("eth_sign", 
+#        rpc_request_output = self.init_rpc_request("eth_sign",
 #                                                    [address, data])
 #        return rpc_request_output['result']
 
