bddf10f849af0d955661ff5c881f8998d0610415
jmantas <jmantas@users.noreply.github.com>
2015-10-29 19:57:01 +0200

    New test

--- test-enic.sh
+++ test-enic.sh
@@ -20,3 +20,4 @@ echo ${COINBASE}
 ./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --blockbyhash 0xeb088bd9560c608aacab338ec24b59623696049ba2baf6d689249bfd9cd06303 
 
 ./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --latestblockinfo
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --code 0x11e18ab77f6ca65a3aa5e7172ee86612ca301053
