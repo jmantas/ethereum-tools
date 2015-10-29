d02c0b2f46f8429f1cb1ed91d7cf616a240912e6
jmantas <jmantas@users.noreply.github.com>
2015-10-29 19:57:13 +0200

    New utility module

---
+++ eth_util.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- mode: python; coding: utf-8 -*-
+
+''' eth utilities module '''
+
+import json
+import yaml
+
+def json_to_yamler(json_dump):
+    json_dumped = json.dumps(json_dump)
+    yamled_dict = yaml.safe_load(json_dumped)
+    return yamled_dict
+
