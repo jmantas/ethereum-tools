3d1014d0433c8da45142d821e29ae7a90fc02ef9
jmantas <jmantas@users.noreply.github.com>
2015-11-04 18:13:11 +0200

    Polishing

--- eth-node-info-cli.py
+++ eth-node-info-cli.py
@@ -15,89 +15,89 @@ def main():
     parser = argparse.ArgumentParser(add_help=True)
     parser.add_argument("--host", dest="jrpc_host")
     parser.add_argument("--port", dest="jrpc_port")
-    parser.add_argument("--version", action="store_true")
-    parser.add_argument("--protocol", action="store_true")
-    parser.add_argument("--networkid", action="store_true")
-    parser.add_argument("--listening", action="store_true")
-    parser.add_argument("--peercount", action="store_true")
-    parser.add_argument("--mining", action="store_true")
-    parser.add_argument("--coinbase", action="store_true")
-    parser.add_argument("--hashrate", action="store_true")
-    parser.add_argument("--gasprice", action="store_true")
-    parser.add_argument("--accounts", action="store_true")
-    parser.add_argument("--blocknumber", action="store_true")
-    parser.add_argument("--balance", dest="account_balance", type=str)
-    parser.add_argument("--txcount", dest="account_txcount", type=str)
-    parser.add_argument("--storage", dest="account_storage", type=str)
-    parser.add_argument("--blockbyhash", dest="block_by_hash", type=str)
-    parser.add_argument("--latestblockinfo", action="store_true")
-    parser.add_argument("--code", dest="code_from_address", type=str)
+    parser.add_argument("--getversion", action="store_true")
+    parser.add_argument("--getprotocol", action="store_true")
+    parser.add_argument("--getnetworkid", action="store_true")
+    parser.add_argument("--islistening", action="store_true")
+    parser.add_argument("--getpeercount", action="store_true")
+    parser.add_argument("--ismining", action="store_true")
+    parser.add_argument("--getcoinbase", action="store_true")
+    parser.add_argument("--gethashrate", action="store_true")
+    parser.add_argument("--getgasprice", action="store_true")
+    parser.add_argument("--getaccounts", action="store_true")
+    parser.add_argument("--getblocknumber", action="store_true")
+    parser.add_argument("--getbalance", dest="getaccount_balance", type=str)
+    parser.add_argument("--gettxcount", dest="getaccount_txcount", type=str)
+    parser.add_argument("--getstorage", dest="getaccount_storage", type=str)
+    parser.add_argument("--getblockbyhash", dest="getblock_by_hash", type=str)
+    parser.add_argument("--getlatestblockinfo", action="store_true")
+    parser.add_argument("--getcode", dest="getcode_from_address", type=str)
     args = parser.parse_args()
 
     
     eth_instance = eth_rpc.EthRPC(args.jrpc_host, args.jrpc_port)
      
-    if args.version:
+    if args.getversion:
         print eth_instance.node_version()
 
-    if args.protocol:
+    if args.getprotocol:
         print eth_instance.protocol_version()
 
-    if args.networkid:
+    if args.getnetworkid:
         print eth_instance.network_id()
 
-    if args.listening:
+    if args.islistening:
         print eth_instance.network_is_listening()
 
-    if args.peercount:
+    if args.getpeercount:
         print eth_instance.network_peer_count()
 
-    if args.mining:
+    if args.ismining:
         print eth_instance.node_is_mining()
 
-    if args.coinbase:
+    if args.getcoinbase:
         print eth_instance.coinbase()
 
-    if args.hashrate:
+    if args.gethashrate:
         print eth_instance.hashrate()
 
-    if args.gasprice:
+    if args.getgasprice:
         print eth_instance.gas_price()
 
-    if args.accounts:
+    if args.getaccounts:
         accounts_json = eth_instance.accounts()
         print eth_util.json_to_yamler(accounts_json)
 
-    if args.blocknumber:
+    if args.getblocknumber:
         print eth_instance.current_block_number()
 
-    if args.account_balance:
-        acc = args.account_balance
+    if args.getaccount_balance:
+        acc = args.getaccount_balance
         balance = eth_instance.balance(acc)
         print balance
 
-    if args.account_txcount:
-        acc = args.account_txcount
+    if args.getaccount_txcount:
+        acc = args.getaccount_txcount
         txcount = eth_instance.transaction_count(acc)
         print txcount
 
-    if args.account_storage:
-        acc = args.account_storage
+    if args.getaccount_storage:
+        acc = args.getaccount_storage
         storage = eth_instance.storage_at(acc)
         print acc, storage
 
-    if args.block_by_hash:
-        block_hash = args.block_by_hash
+    if args.getblock_by_hash:
+        block_hash = args.getblock_by_hash
         info = eth_instance.block_info_by_hash(block_hash)
         print info
 
-    if args.latestblockinfo:
+    if args.getlatestblockinfo:
         block_number = "latest"
         latestblockinfo_json = eth_instance.block_info_by_number(block_number)
         print eth_util.json_to_yamler(latestblockinfo_json)
     
-    if args.code_from_address:
-        address = args.code_from_address
+    if args.getcode_from_address:
+        address = args.getcode_from_address
         info = eth_instance.code_from_address_hash(address)
         print info
 
--- test-enic.sh
+++ test-enic.sh
@@ -2,22 +2,22 @@
 #
 # Little Tester script
 
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --version
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --protocol
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --networkid
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --listening
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --peercount
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --mining
-COINBASE=$(./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --coinbase)
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --getversion
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --getprotocol
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --getnetworkid
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --islistening
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --getpeercount
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --ismining
+COINBASE=$(./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --getcoinbase)
 echo ${COINBASE}
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --hashrate
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --gasprice
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --accounts
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --blocknumber
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --balance ${COINBASE}
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --txcount ${COINBASE}
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --storage ${COINBASE}
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --blockbyhash 0xeb088bd9560c608aacab338ec24b59623696049ba2baf6d689249bfd9cd06303 
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --gethashrate
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --getgasprice
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --getaccounts
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --getblocknumber
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --getbalance ${COINBASE}
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --gettxcount ${COINBASE}
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --getstorage ${COINBASE}
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --getblockbyhash 0xeb088bd9560c608aacab338ec24b59623696049ba2baf6d689249bfd9cd06303 
 
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --latestblockinfo
-./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --code 0x11e18ab77f6ca65a3aa5e7172ee86612ca301053
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --getlatestblockinfo
+./eth-node-info-cli.py --host 127.0.0.1 --port 18801 --getcode 0x11e18ab77f6ca65a3aa5e7172ee86612ca301053
