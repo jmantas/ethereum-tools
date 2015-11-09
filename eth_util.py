cd7d6710f289c38b4c01c4d4a1073445cd6d43c7
jmantas <jmantas@users.noreply.github.com>
2015-11-09 20:35:38 +0200

    Readability

--- eth_util.py
+++ eth_util.py
@@ -7,6 +7,7 @@ import json
 import yaml
 
 def json_to_yamler(json_dump):
+    ''' converts from json to yaml output '''
     json_dumped = json.dumps(json_dump)
     yamled_dict = yaml.safe_load(json_dumped)
     return yamled_dict
